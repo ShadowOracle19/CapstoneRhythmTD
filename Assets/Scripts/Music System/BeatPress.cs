2345585eabe8c95e22b4b1c91f04c35925e02be3
Lucy Coates <47534892+ShadowOracle19@users.noreply.github.com>
2024-10-29 20:22:41 -0400

    created new conductor class

---
+++ Assets/Scripts/Music System/BeatPress.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeatPress : MonoBehaviour
+{
+    public Image beatIndicator;
+
+    public GameObject miss;
+    public GameObject good;
+    public GameObject perfect;
+
+    public bool inputPressed;
+
+    public float lastBeat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastBeat = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beatIndicator.color = new Color(1, 1, 1, ConductorV2.instance.loopPositionInAnalog);
+
+        
+        
+        if ((ConductorV2.instance.songPosition - lastBeat) / ConductorV2.instance.crotchet == 1)
+        {
+            Debug.Log("hit");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //miss happens between 0 and 0.3, good happens between 0.31 and 0.6, perfect happens between 0.61 and 1
+            if (ConductorV2.instance.loopPositionInAnalog <= 0.3) //miss
+            {
+                Debug.Log("miss");
+                miss.SetActive(true);
+                good.SetActive(false);
+                perfect.SetActive(false);
+            }
+            else if(ConductorV2.instance.loopPositionInAnalog < 0.6 && ConductorV2.instance.loopPositionInAnalog > 0.31) //good
+            {
+                Debug.Log("good");
+                miss.SetActive(false);
+                good.SetActive(true);
+                perfect.SetActive(false);
+            }
+            else if(ConductorV2.instance.loopPositionInAnalog < 1 && ConductorV2.instance.loopPositionInAnalog > 0.61)
+            {
+                Debug.Log("perfect");
+                miss.SetActive(false);
+                good.SetActive(false);
+                perfect.SetActive(true);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Music System/ConductorV2.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConductorV2 : MonoBehaviour
+{
+    //Conductor instance
+    public static ConductorV2 instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public float bpm = 160;//song beats per minute
+    public float crotchet;//Gives the time duration of a beat, calculated from the bpm
+    public float songPosition;
+    public float songPositionInBeats;//current song position in beats
+    public float dspSongTime;//how many seconds have passed since the song started
+    public AudioSource musicSource;
+
+    public float offset;
+
+    //The number of beats in each loop
+    public float beatsPerLoop;
+
+    //the total number of loops completed since the looping clip first started
+    public int completedLoops = 0;
+
+    //The current position of the song within the loop in beats.
+    public float loopPositionInBeats;
+
+    //The current relative position of the song within the loop measured between 0 and 1.
+    public float loopPositionInAnalog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //load the audio source attached to the conductor gameobject
+        musicSource = GetComponent<AudioSource>();
+
+        //calculate the number of seconds in each beat
+        crotchet = 60 / bpm;
+
+        //record the time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //Start the song
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //determine how many seconds since the song started
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - offset);
+
+        //determine how many beats since the song started
+        songPositionInBeats = songPosition / crotchet;
+
+        //calculate the loop position
+        if (songPositionInBeats >= (completedLoops + 1) * beatsPerLoop)
+            completedLoops++;
+        loopPositionInBeats = songPositionInBeats - completedLoops * beatsPerLoop;
+
+        loopPositionInAnalog = loopPositionInBeats / beatsPerLoop;
+
+    }
+}
+

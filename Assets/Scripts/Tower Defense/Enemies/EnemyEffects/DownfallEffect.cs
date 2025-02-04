6a686f9a9ea455d90d4c23b2b467e090519d4389
Lucy Coates <47534892+ShadowOracle19@users.noreply.github.com>
2025-02-04 15:15:22 -0500

    charmed and downfall prefab creation, update wisp

---
+++ Assets/Scripts/Tower Defense/Enemies/EnemyEffects/DownfallEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownfallEffect : EnemyEffect
+{
+    public GameObject downfallProjectile;
+    public override void UseEffect()
+    {
+        base.UseEffect();
+        
+
+    }
+}
